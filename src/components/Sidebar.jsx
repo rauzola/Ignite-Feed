780bb4df2d7725f9f0108797a4c6f1e25c5f10cf
Raul - Anpad <raul_sigoli@hotmail.com>
2022-06-12 18:37:27 -0300

    Finalizando Sidebar
    Nessa aula continuaremos o desenvolvimento da Sidebar que começamos na aula anterior, e finalizaremos a estilização desse componente.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -9,12 +11,18 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/57502280?v=4" 
+        />
+
         <strong>Raul Sigoli</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine  size={20} />
           Editar seu perfil
         </a>
       </footer>
