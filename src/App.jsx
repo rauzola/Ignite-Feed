42514dd75de8c483605a43670fa196b850d0997f
Raul - Anpad <raul_sigoli@hotmail.com>
2022-06-12 17:13:40 -0300

    Componente: Sidebar
    Agora vamos começar a criar a estrutura do corpo da aplicação, que é onde exibiremos a Sidebar e o Feed de posts, e iniciar a construção e estilização do componente de Sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
-    <Header />
-      <h1>Hello World</h1>
-      <Post 
-        author="Raul Sigoli" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, perspiciatis! Dicta vero id enim ipsa nemo! Exercitationem beatae facere harum. Incidunt vero perferendis et tenetur nobis est beatae vitae dignissimos." 
-      />
-      <Post 
-        author="Ruan Sigoli" 
-        content="Um post muito legal."
-      />
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Raul Sigoli"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, perspiciatis! Dicta vero id enim ipsa nemo! Exercitationem beatae facere harum. Incidunt vero perferendis et tenetur nobis est beatae vitae dignissimos."
+          />
+          <Post
+            author="Ruan Sigoli"
+            content="Um post muito legal."
+          />
+        </main>
+      </div>
+
     </>
   )
 }
