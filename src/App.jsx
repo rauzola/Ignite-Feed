3ccf165eda03cef7f070415d89c2581cf599ab67
Raul - Anpad <raul_sigoli@hotmail.com>
2022-06-15 15:14:05 -0300

    Iterando no JSX
    Agora que temos toda a estrutura e estilização da nossa aplicação, nós precisamos dar vida e funcionalidade à ela.
    
    Nessa aula vamos começar a aprender sobre como estruturar os dados do nosso elemento e iterar esses dados para exibir itens de maneira dinâmica na tela.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from "./App.module.css";
 
 import './global.css';
 
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/rauzola.png",
+      name: "Raul Sigoli",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -15,14 +46,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Raul Sigoli"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, perspiciatis! Dicta vero id enim ipsa nemo! Exercitationem beatae facere harum. Incidunt vero perferendis et tenetur nobis est beatae vitae dignissimos."
-          />
-          <Post
-            author="Ruan Sigoli"
-            content="Um post muito legal."
-          />
+          {post.map(post => {
+            return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+          })}
         </main>
       </div>
 
