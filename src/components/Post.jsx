100bbd93a6fa8da257bd3cbca2b203dc112c78f8
Raul - Anpad <raul_sigoli@hotmail.com>
2022-06-16 00:23:14 -0300

    Inserindo comentários
    Agora vamos adicionar ainda mais funcionalidade aos nossos comentários dos Posts.
    
    Vamos aprender como utilizar do conceito de formulários e componentes controlados do ReactJS para podermos adicionar comentários com textos baseados no que digitamos no input de cada post.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,9 +8,10 @@ import styles from './Post.module.css';
 
 export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
-    1,
-    2,
-  ])
+    'Post muito bacana, hein?!'
+  ]);
+
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -24,9 +25,14 @@ export function Post({ author, publishedAt, content }) {
   function handleCreateNewComment() {
     event.preventDefault()
 
-    setComments([...comments, comments.length + 1]);
+   
 
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
 
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
 
   return (
@@ -47,19 +53,24 @@ export function Post({ author, publishedAt, content }) {
       </header>
 
       <div className={styles.content}>
-       {content.map(line =>{
-        if(line.type === 'paragraph') {
-          return <p>{line.content}</p>;
-        } else if (line.type === 'link') {
-          return <p><a href="#">{line.content}</a></p>
-        }
-       })}
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>
 
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          name="comment"
+          placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+        />
 
         <footer>
           <button type="submit">Publicar</button>
@@ -68,7 +79,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
 
