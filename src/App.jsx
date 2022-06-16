116b1b520bf4fbc3c62bb5c01da0782e2c3d6288
Raul - Anpad <raul_sigoli@hotmail.com>
2022-06-15 22:49:59 -0300

    Propriedades do Post
    Na última aula nós fizemos a iteração de um array de posts. Já nessa aula vamos aprender a como utilizar essa iteração para enviar os dados de cada post de nosso array para componente Post para que ele possa ser renderizado em tela dinâmicamente.

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const post = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
+    publishedAt: new Date('2022-06-03 20:00:00'),
   },
   {
     id: 2,
@@ -33,7 +33,7 @@ const post = [
       { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀', },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
+    publishedAt: new Date('2022-06-10 20:00:00'),
   },
 ];
 
